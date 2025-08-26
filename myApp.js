c228dc3e96c17b1a628121a63bca3ad4f00a3fd7
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-26 14:46:33 +0200

    Refactor helmet for simplicity

--- myApp.js
+++ myApp.js
@@ -2,25 +2,36 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({ action: 'deny' }));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
 
-const timeInSeconds = 90*24*60*60;
-app.use(helmet.hsts({
-  maxAge: timeInSeconds, force: true}));
+// const timeInSeconds = 90*24*60*60;
+// app.use(helmet.hsts({
+//   maxAge: timeInSeconds, force: true}));
 
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", 'trusted-cdn.com'], 
-  }
-})
-);
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", 'trusted-cdn.com'], 
+//   }
+// })
+// );
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+    }
+  },
+  noCache: true
+
+}))
 
 
 
@@ -51,6 +62,7 @@ app.use(helmet.contentSecurityPolicy({
 
 module.exports = app;
 const api = require('./server.js');
+// const { act } = require('react');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
